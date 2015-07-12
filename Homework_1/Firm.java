8230904f4b6c393890ba551fa1206ac98849a404
GerganaG <gergana071@gmail.com>
2015-07-12 13:02:42 +0300

    Correct_Homework_1

--- Homework_1/Firm.java
+++ Homework_1/Firm.java
@@ -6,34 +6,34 @@ public class Firm {
 		
 		Scanner input = new Scanner(System.in, "UTF-8");
 		
-		String FirstName, LastName;
-		long Age, PersonalNum, TelephoneNum;
-		char Gender;
+		String firstName, lastName;
+		long age, personalNum, telephoneNum;
+		char gender;
 		
 		System.out.println("Enter First Name: ");
-		FirstName = input.nextLine();
+		firstName = input.nextLine();
 		
 		System.out.println("Enter Last Name: ");
-		LastName = input.nextLine();
+		lastName = input.nextLine();
 		
 		System.out.println("Enter Age: ");
-		Age = input.nextLong();
+		age = input.nextLong();
 		
 		System.out.println("Enter Gender (M/F only): ");
-		Gender = input.next().charAt(0);
+		gender = input.next().charAt(0);
 		
 		System.out.println("Enter Personal Number: ");
-		PersonalNum = input.nextLong();
+		personalNum = input.nextLong();
 		
-		System.out.println("Enter Telephone Number: ");
-		TelephoneNum = input.nextLong();
+		System.out.println("Enter Telephone Number +359: ");
+		telephoneNum = input.nextLong();
 		
-		System.out.println("Enterd First Name: " + FirstName);
-		System.out.println("Enterd Last Name: " + LastName);
-		System.out.println("Enterd Age: " + Age);
-		System.out.println("Enterd Gender: " + Gender);
-		System.out.println("Enterd Personal Number: " + PersonalNum);
-		System.out.println("Enterd Telephone Number: +359" + TelephoneNum);
+		System.out.println("Enterd First Name: " + firstName);
+		System.out.println("Enterd Last Name: " + lastName);
+		System.out.println("Enterd Age: " + age);
+		System.out.println("Enterd Gender: " + gender);
+		System.out.println("Enterd Personal Number: " + personalNum);
+		System.out.println("Enterd Telephone Number: +359" + telephoneNum);
 		
 		input.close();
 		
